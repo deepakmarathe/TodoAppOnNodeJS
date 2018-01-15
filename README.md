74e4ff71c3f20cdde297bd6160ce0eaef8ce415e
deepu <deepu@deepus-MacBook-Pro.local>
2018-01-16 02:41:16 +0530

    adding delete operation in the todoController

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TodoAppOnNodeJS
 Todo app built using nodes
+
+Setup : 
+    npm init
+
+    npm install express -save
+    npm install body-parser -save
+
+    eslint --init
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,22 @@
+var bodyParser = require('body-parser')
+
+var data = [{item: 'get milk'}, {item: 'walk dog'},{item: 'kick some coding ass'}]
+var urlencodedParser = bodyParser.urlencoded({extended : false})
+
 module.exports = function(app){
     app.get('/todo', function(request, response){
-        response.render('todo')        
+        response.render('todo', {todos: data})        
     })    
 
-    app.post('/todo', function(request, response){
-
+    app.post('/todo', urlencodedParser, function(request, response){
+        data.push(request.body)
+        response.json(data)
     })    
     
-    app.delete('/todo', function(request, response){
-        
+    app.delete('/todo/:item', function(request, response){
+        data = data.filter(function(todo){
+            return todo.item !== request.params.item
+        })
+        response.json(data)
     })    
 }
