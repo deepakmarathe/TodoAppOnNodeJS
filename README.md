9fbb9acc8cdfeb30a49b50aa0c38a51623a293b8
deepu <deepu@deepus-MacBook-Pro.local>
2018-01-21 17:29:07 +0530

    hooking it up with database

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ Setup :
 
     npm install express -save
     npm install body-parser -save
+    npm install mongoose -save
 
     eslint --init
+
+Resources : 
+    The Net Ninja : Nodejs tutorials (37 parts)
+    https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg
+    
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,44 @@
 var bodyParser = require('body-parser')
 
-var data = [{item: 'get milk'}, {item: 'walk dog'},{item: 'kick some coding ass'}]
+var mongoose = require('mongoose')
+
+//Connect to the database
+mongoose.connect('mongodb://dbadmin:dbadmin@ds159187.mlab.com:59187/nodetodotest')
+
+//Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+	item: String
+})
+
+var Todo = mongoose.model('Todo', todoSchema)
+
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'},{item: 'kick some coding ass'}]
 var urlencodedParser = bodyParser.urlencoded({extended : false})
 
 module.exports = function(app){
-    app.get('/todo', function(request, response){
-        response.render('todo', {todos: data})        
-    })    
-
-    app.post('/todo', urlencodedParser, function(request, response){
-        data.push(request.body)
-        response.json(data)
-    })    
-    
-    app.delete('/todo/:item', function(request, response){
-        data = data.filter(function(todo){
-            return todo.item !== request.params.item
+	app.get('/todo', function(request, response){
+        Todo.find({}, function(err, data){
+            if(err) throw err
+            response.render('todo', {todos: data})        
         })
+	})    
+
+	app.post('/todo', urlencodedParser, function(request, response){
+        //get data from the view and add it to mongodb
+        var newTodo = Todo(request.body).save(function(err, data){
+            if( err) throw err
+            response.json(data)
+        })
+		// data.push(request.body)
+		// response.json(data)
+	})    
+    
+	app.delete('/todo/:item', function(request, response){
+        // delete the requested item from mongodb
+        Todo.find({item: request.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if( err) throw err
         response.json(data)
-    })    
+        })
+	})    
 }
